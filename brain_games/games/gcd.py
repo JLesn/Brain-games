3a13798f8b5e44d4fe7857e47e293a2c1a08541d
JLesn <lesnikova.j00@gmail.com>
2023-10-21 00:30:30 +0500

    remade games

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -1,30 +1,20 @@
-import prompt
 from random import randint
 from math import gcd
 
 
-def game_gcd():
-    print('Welcome to the Brain Games!')
-    name = prompt.string('May I have your name ? ')
-    print(f'Hello, {name}!')
-    print('Find the greatest common divisor of given numbers.')
-    counter = 0
-    while counter != 3:
-        counter += 1
-        num1 = randint(1, 100)
-        num2 = randint(1, 100)
-        res = str(gcd(num1, num2))
-        print(f'Question: {num1} {num2}')
-        answ = input()
-        print(f'Your answer: {answ}')
-        if res == answ:
-            print('Correct!')
-        else:
-            print(
-                f'\'{answ}\' is wrong answer ;(.'
-                f' Correct answer was \'{res}\'.'
-            )
-            print(f'Let\'s try again, {name}!')
-            break
-        if counter == 3:
-            print(f'Congratulations, {name}!')
+def game():
+  print('Find the greatest common divisor of given numbers.')
+  num1 = randint(1, 100)
+  num2 = randint(1, 100)
+  result = str(gcd(num1, num2))
+  print(f'Question: {num1} {num2}')
+  answ = input()
+  print(f'Your answer: {answ}')
+  if result == answ:
+    return True
+  else:
+    print(
+      f'\'{answ}\' is wrong answer ;(.'
+      f' Correct answer was \'{result}\'.'
+    )
+    return False
--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -1,34 +1,24 @@
 from random import randint
-import prompt
 
 
-def game_progression():
-    print('Welcome to the Brain Games!')
-    name = prompt.string('May I have your name ? ')
-    print(f'Hello, {name}!')
-    print('What number is missing in the progression?')
-    counter = 0
-    while counter != 3:
-        counter += 1
-        step = randint(1, 10)
-        position = randint(0, 9)
-        stop = randint(0, 100)
-        start = stop - step * 10
-        lst = [i for i in range(start, stop, step)]
-        hidden = str(lst[position])
-        lst[position] = '..'
-        given = ' '.join(str(el) for el in lst)
-        print(f'Question: {given}')
-        answ = input()
-        print(f'Your answer: {answ}')
-        if hidden == answ:
-            print('Correct!')
-        else:
-            print(
-                f'\'{answ}\' is wrong answer ;(.'
-                f' Correct answer was \'{hidden}\'.'
-            )
-            print(f'Let\'s try again, {name}!')
-            break
-        if counter == 3:
-            print(f'Congratulations, {name}!')
+def game():
+  print('What number is missing in the progression?')
+  step = randint(1, 10)
+  position = randint(0, 9)
+  stop = randint(0, 100)
+  start = stop - step * 10
+  lst = [i for i in range(start, stop, step)]
+  hidden = str(lst[position])
+  lst[position] = '..'
+  given = ' '.join(str(el) for el in lst)
+  print(f'Question: {given}')
+  answ = input()
+  print(f'Your answer: {answ}')
+  if hidden == answ:
+    return True
+  else:
+    print(
+      f'\'{answ}\' is wrong answer ;(.'
+      f' Correct answer was \'{hidden}\'.'
+    )
+    return False
