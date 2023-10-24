d2d014e638178253920176b9d6a0fd64901cf0a6
JLesn <lesnikova.j00@gmail.com>
2023-10-24 05:19:54 +0500

    Fix gcd.py

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -7,6 +7,6 @@ MESSAGE = 'Find the greatest common divisor of given numbers.'
 def qw_and_answ():
     num1 = randint(1, 100)
     num2 = randint(1, 100)
-    qw = f'{num1}{num2}'
+    qw = f'{num1} {num2}'
     result = str(gcd(num1, num2))
     return qw, result
