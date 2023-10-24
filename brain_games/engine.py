080dff6b75433ef6d57a09bf10f0411a0ece2557
JLesn <lesnikova.j00@gmail.com>
2023-10-24 05:02:03 +0500

    Fix engine

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -1,17 +1,24 @@
 import prompt
 
+ROUNDS = 3
+
 
 def run_game(game_name):
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name ? ')
     print(f'Hello, {name}!')
-    counter = 0
-    while counter != 3:
-        counter += 1
-        res = game_name.game()
-        if res:
-            print('Correct!')
-            print(f'Congratulations, {name}!')
-        if not res:
-            print(f'Let\'s try again, {name}!')
+    print(game_name.MESSAGE)
+    count_rounds = 1
+    while count_rounds <= ROUNDS:
+        qw, answ = game_name.qw_and_answ()
+        print(f'Question: {qw}')
+        user_answ = prompt.string('Your answer: ')
+        if not (user_answ == answ):
+            print(f"'{user_answ}' is wrong answer ;(. "
+                  f"Correct answer was '{answ}'.\n"
+                  f"Let\'s try again, {name}!")
             break
+        print('Correct!')
+        count_rounds += 1
+    else:
+        print(f'Congratulations, {name}!')
